540d39e5df8481970fd9d22f67fbce17958e4208
panostp <63464224+panostp@users.noreply.github.com>
2026-09-12 17:26:55 +0300

    Delete src directory

--- src/register_types.h
+++
@@ -1,7 +0,0 @@
-#ifndef HELLOWORLD_REGISTER_TYPES_H
-#define HELLOWORLD_REGISTER_TYPES_H
-
-void register_hello_world_types();
-void unregister_hello_world_types();
-
-#endif // HELLOWORLD_REGISTER_TYPES_H
